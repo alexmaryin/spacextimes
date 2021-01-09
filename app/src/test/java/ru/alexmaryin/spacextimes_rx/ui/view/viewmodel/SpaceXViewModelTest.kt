ad284d7d65c502b6a97d94f7cc54cefc44610177
java73 <java73@yandex.ru>
2021-01-09 13:34:00 +0400

    Translate callback for lastUpdate simplified and improved

---
+++ app/src/test/java/ru/alexmaryin/spacextimes_rx/ui/view/viewmodel/SpaceXViewModelTest.kt
@@ -0,0 +1,45 @@
+package ru.alexmaryin.spacextimes_rx.ui.view.viewmodel
+
+import org.junit.Test
+import ru.alexmaryin.spacextimes_rx.data.model.Capsule
+import ru.alexmaryin.spacextimes_rx.data.model.CapsuleStatus
+import ru.alexmaryin.spacextimes_rx.data.model.CapsuleType
+import java.util.*
+
+class SpaceXViewModelTest {
+
+    private fun testCapsulesList(isNullInLastUpdate: Boolean) = mutableListOf<Capsule>().apply {
+        (0..9).forEach {
+            add(
+                Capsule(
+                    serial = "10$it",
+                    lastUpdate = when (isNullInLastUpdate) {
+                        true -> null
+                        false -> "Test of translator"
+                    },
+                    type = CapsuleType.values().random(),
+                    status = CapsuleStatus.values().random(),
+                    id = UUID.randomUUID(),
+                    reuseCount = Random().nextInt(10),
+                    waterLandings = Random().nextInt(10),
+                    landLandings = 0,
+                    lastUpdateRu = null
+                )
+            )
+        }
+    }
+
+    @Test
+    fun translateCapsulesLastUpdateInitialNull() {
+        val testList = testCapsulesList(true)
+        print(testList)
+        assert(true)
+    }
+
+    @Test
+    fun translateCapsulesLastUpdateNotEmpty() {
+        val testList = testCapsulesList(false)
+        print(testList)
+        assert(true)
+    }
+}
