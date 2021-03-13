6eac3898f1b61466fdebd7aa8526f95d08cb3b10
alexmaryin <java73@yandex.ru>
2021-03-13 22:02:26 +0400

    Translator for wiki-links has been featured

--- app/src/test/java/ru/alexmaryin/spacextimes_rx/data/api/wiki/WikiLoaderImplTest.kt
+++ app/src/test/java/ru/alexmaryin/spacextimes_rx/data/api/wiki/WikiLoaderImplTest.kt
@@ -1,16 +1,15 @@
 package ru.alexmaryin.spacextimes_rx.data.api.wiki
 
-import org.junit.Assert.*
-import org.junit.Before
-
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class WikiLoaderImplTest {
 
-   private val wikiApi = WikiLoaderImpl()
+    private val wikiApi = WikiLoaderImpl()
 
     @Test
-    fun `wiki link translator should return correct link`() {
+    fun `wiki link translator should return correct link`() = runBlocking {
         val origin = "https://en.wikipedia.org/wiki/Bob_Behnken"
         val expected = "https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D0%BD%D0%BA%D0%B5%D0%BD,_%D0%A0%D0%BE%D0%B1%D0%B5%D1%80%D1%82"
         val locale = "ru"
@@ -19,7 +18,7 @@ class WikiLoaderImplTest {
     }
 
     @Test
-    fun `wiki link translator should return origin link if specified is avoid`() {
+    fun `wiki link translator should return origin link if specified is avoid`() = runBlocking {
         val origin = "https://en.wikipedia.org/wiki/Bob_Behnken"
         val expected = "https://en.wikipedia.org/wiki/Bob_Behnken"
         val locale = "гы"
